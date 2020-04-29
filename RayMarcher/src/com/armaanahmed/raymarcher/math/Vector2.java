4d327e4df4cc40fa07fda9e6e866ab894b511afc
Armaan Ahmed <aahmed22@scarsdaleschools.org>
2020-04-29 11:57:53 -0400

    Implemented the cross product and implemented Vector3's own ZERO (and the distance from zero function)

--- RayMarcher/src/com/armaanahmed/raymarcher/math/Vector2.java
+++ RayMarcher/src/com/armaanahmed/raymarcher/math/Vector2.java
@@ -2,7 +2,9 @@ package com.armaanahmed.raymarcher.math;
 
 public class Vector2 extends Vector<Vector2> {
 	
-	public double x, y;
+	public static final Vector2 ZERO = new Vector2(0,0);
+	
+	private double x, y;
 	
 	public Vector2(double _x, double _y) {
 		x = _x;
@@ -20,18 +22,41 @@ public class Vector2 extends Vector<Vector2> {
 	}
 
 	@Override
-	public Vector2 mult(Vector2 v) {
-		return new Vector2(x * v.x, y * v.y);
+	public Vector2 mult(double num) {
+		return new Vector2(x * num, y * num);
 	}
 
 	@Override
-	public Vector2 div(Vector2 v) {
-		return new Vector2(x / v.x, y / v.y);
+	public Vector2 div(double num) {
+		return new Vector2(x / num, y / num);
 	}
 
 	@Override
 	public double distance(Vector2 v) {
 		return Math.sqrt( Math.pow(x - v.x, 2) + Math.pow(y - v.y, 2));
+	}
+
+	@Override
+	public double dot(Vector2 v) {
+		return x*v.x + y*v.y;
+	}
+
+	@Override
+	public Vector2 cross(Vector2 v) {
+		return null;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+
+	@Override
+	public double distance() {
+		return distance(ZERO);
 	}
 
 }
