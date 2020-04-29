cb8f5bfc0ddb0321bf34311b614630bfb78e4b11
Armaan Ahmed <aahmed22@scarsdaleschools.org>
2020-04-29 11:59:28 -0400

    Removed zero, fixed mistake so that mult and div take in doubles, added dot product, cross product, and distance (from zero) abstract methods. normalize also added

--- RayMarcher/src/com/armaanahmed/raymarcher/math/Vector.java
+++ RayMarcher/src/com/armaanahmed/raymarcher/math/Vector.java
@@ -1,12 +1,19 @@
 package com.armaanahmed.raymarcher.math;
 
-public abstract class Vector<E> {
+public abstract class Vector<E extends Vector<E>> {
 	
 	public abstract E add(E v);
 	public abstract E sub(E v);
-	public abstract E mult(E v);
-	public abstract E div(E v);
+	public abstract E mult(double num);
+	public abstract E div(double num);
 	
 	public abstract double distance(E v);
+	public abstract double dot(E v);
+	public abstract E cross(E v);
+	
+	public abstract double distance();
+	public final E normalize() {
+		return mult(1/distance());
+	}
 
 }
